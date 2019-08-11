89693ff99a99d47a749bd3ce1325f0ab4652b2ee
ayerest <irisiseve@gmail.com>
2019-08-10 18:21:29 -0700

    finished rails edit update action lab minus the extra credit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  # patch 'articles/:id', to: 'articles#update'
+  #line below is the same as the lines above (with extra routes)
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
+
 end
